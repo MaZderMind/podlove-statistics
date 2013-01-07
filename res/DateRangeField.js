aa9c3ddad80a4497a5c264be9b31139c8b9b82a9
Peter <peter@mazdermind.de>
2013-01-07 23:11:57 +0100

    refactor date field to get internal state and change events out

--- res/DateRangeField.js
+++ res/DateRangeField.js
@@ -1,19 +1,53 @@
 Ext.define('Ext.ux.form.field.DateRange', {
 	extend: 'Ext.form.TriggerField',
-    alias: 'widget.daterangefield',
+ 	alias: 'widget.daterangefield',
 
 	strBefore: l18n.DateRangePicker.BeforePrefix,
 	strAfter: l18n.DateRangePicker.AfterPrefix,
+	strBetween: l18n.DateRangePicker.BetweenInfix,
 	displayFormat: l18n.DateRangePicker.DisplayFormat,
 	//width: 175,
 	triggerCls: 'x-form-date-trigger',
 	fromDate: null,
 	toDate: null,
+	internalValue: null,
+
+	setInternalValue: function(value) {
+		if(!this.fireEvent('beforeInternalValueChange', value))
+			return;
+
+		this.internalValue = value;
+
+		if(value.relative) {
+			this.setValue(l18n.DateRangePicker.Menu[value.relative]);
+		}
+		else if(value.atdate) {
+			this.setValue(Ext.Date.format(value.atdate, this.displayFormat));
+		}
+		else if(value.before) {
+			this.setValue(this.strBefore + Ext.Date.format(value.before, this.displayFormat));
+		}
+		else if(value.after) {
+			this.setValue(this.strAfter + Ext.Date.format(value.after, this.displayFormat));
+		}
+		else if(value.from && value.to) {
+			this.setValue(Ext.Date.format(value.from, this.displayFormat) + this.strBetween + Ext.Date.format(value.to, this.displayFormat));
+		}
+		else {
+			this.setValue('???');
+		}
+
+		this.fireEvent('afterInternalValueChange');
+	},
+
+	getInternalValue: function() {
+		return this.internalValue;
+	},
 
 	getDateRange: function() {
 		return [this.fromDate, this.toDate];
 	},
-	
+
 	onTriggerClick: function() {
 		if(this.disabled){
 			return;
@@ -28,25 +62,23 @@ Ext.define('Ext.ux.form.field.DateRange', {
 				ignoreParentClicks :true,
 				width: this.el.getWidth(),
 				items: [
+					// relative values, that refer to an relative date ranges
 					{
 						text: l18n.DateRangePicker.Menu.Today,
-						handler: Ext.bind(this.setValue, this, [format(new Date())])
+						handler: Ext.bind(this.setInternalValue, this, [{relative: 'Today'}])
 					}, {
 						text: l18n.DateRangePicker.Menu.Last7Days,
-						handler: Ext.bind(this.setValue, this, [
-							format(Ext.Date.add(new Date(), Ext.Date.DAY, -7)) + ' - ' + format(new Date())
-						])
+						handler: Ext.bind(this.setInternalValue, this, [{relative: 'Last7Days'}])
 					}, {
 						text: l18n.DateRangePicker.Menu.ThisMonth,
-						handler: Ext.bind(this.setValue, this, [
-							format(Ext.Date.getFirstDateOfMonth(new Date()) ) + ' - ' + format(new Date())
-						])
+						handler: Ext.bind(this.setInternalValue, this, [{relative: 'ThisMonth'}])
 					}, {
 						text: l18n.DateRangePicker.Menu.ThisYear,
-						handler: Ext.bind(this.setValue, this, [
-							format(new Date(new Date().getFullYear(), 0, 1)) + ' - ' + format(new Date(new Date().getFullYear(), 11, 31))
-						])
-					}, {
+						handler: Ext.bind(this.setInternalValue, this, [{relative: 'ThisYear'}])
+					},
+
+					// absolute values, thet refer to an absolute date range
+					{
 						text: l18n.DateRangePicker.Menu.AtDate,
 						menu: {
 							plain: true,
@@ -55,7 +87,7 @@ Ext.define('Ext.ux.form.field.DateRange', {
 								margins: '10 10 10 10',
 								listeners: {
 									select: Ext.bind(function(p, dt) {
-										this.setValue(format(dt))
+										this.setInternalValue({atdate: dt});
 									}, this)
 								}
 							}
@@ -69,7 +101,7 @@ Ext.define('Ext.ux.form.field.DateRange', {
 								margins: '10 10 10 10',
 								listeners: {
 									select: Ext.bind(function(p, dt) {
-										this.setValue(this.strBefore + ' ' + format(dt))
+										this.setInternalValue({before: dt});
 
 										this.fromDate = dt;
 										this.toDate = null;
@@ -86,7 +118,7 @@ Ext.define('Ext.ux.form.field.DateRange', {
 								margins: '10 10 10 10',
 								listeners: {
 									select: Ext.bind(function(p, dt) {
-										this.setValue(this.strAfter + ' ' + format(dt))
+										this.setInternalValue({after: dt})
 										
 										this.fromDate = null;
 										this.toDate = dt;
@@ -110,7 +142,7 @@ Ext.define('Ext.ux.form.field.DateRange', {
 											select: Ext.bind(function(p, dt) {
 												var toPicker = p.ownerCt.getComponent('toPicker');
 												toPicker.setMinDate(dt);
-												this.setValue(format(dt) + ' - ' + format(toPicker.getValue()))
+												this.setInternalValue({from: dt, to: toPicker.getValue()});
 
 												this.fromDate = dt;
 												this.toDate = toPicker.getValue();
@@ -124,7 +156,7 @@ Ext.define('Ext.ux.form.field.DateRange', {
 											select: Ext.bind(function(p, dt) {
 												var fromPicker = p.ownerCt.getComponent('fromPicker');
 												fromPicker.setMaxDate(dt);
-												this.setValue(format(fromPicker.getValue()) + ' - ' + format(dt))
+												this.setInternalValue({from: fromPicker.getValue(), to: dt});
 
 												this.fromDate = fromPicker.getValue();
 												this.toDate = dt;
